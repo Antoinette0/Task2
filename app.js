30ef21ba3fb1206971b6037cb4e736bd769f1816
Antoinette0 <atolagbeoyinantoinette@gmail.com>
2021-04-11 16:30:19 +0100

    added index.html, javascript and styles .css

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const navLINKS = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLINKS.classList.toggle('show');
+});
